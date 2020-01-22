aa3f92404c81f31ddf539363cfcbab49ce8913cf
Speterius <peet.seres@gmail.com>
2020-01-22 13:31:43 +0100

    Added gui elements to the game engine.

---
+++ arcade_game/gui_elements.py
@@ -0,0 +1,131 @@
+import arcade
+import typing
+
+
+class Button:
+    def __init__(self, center_x, center_y, width, height, action_function):
+        self.center_x = center_x
+        self.center_y = center_y
+        self.width = width
+        self.height = height
+        self.action_function = action_function
+        self.pressed = False
+
+    def on_press(self):
+        self.pressed = True
+
+    def on_release(self):
+        self.pressed = False
+        self.action_function()
+
+    def check_mouse_press(self, x, y):
+        if x > self.center_x + self.width / 2:
+            return False
+        if x < self.center_x - self.width / 2:
+            return False
+        if y > self.center_y + self.height / 2:
+            return False
+        if y < self.center_y - self.height / 2:
+            return False
+        return True
+
+
+class TextBoxButton(Button):
+    def __init__(self, text, center_x, center_y, width, height, action_function,
+                 font_size=18,
+                 font_face="Arial",
+                 face_color=arcade.color.LIGHT_GRAY,
+                 highlight_color=arcade.color.WHITE,
+                 shadow_color=arcade.color.GRAY,
+                 button_height=2):
+        super().__init__(center_x, center_y, width, height, action_function)
+
+        self.text = text
+        self.font_size = font_size
+        self.font_face = font_face
+        self.face_color = face_color
+        self.highlight_color = highlight_color
+        self.shadow_color = shadow_color
+        self.button_height = button_height
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.center_x, self.center_y, self.width, self.height, self.face_color)
+
+        if not self.pressed:
+            color = self.shadow_color
+        else:
+            color = self.highlight_color
+
+        # Bottom horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         color, self.button_height)
+
+        # Right vertical
+        arcade.draw_line(self.center_x + self.width / 2, self.center_y - self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        if not self.pressed:
+            color = self.highlight_color
+        else:
+            color = self.shadow_color
+
+        # Top horizontal
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         self.center_x + self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        # Left vertical
+        arcade.draw_line(self.center_x - self.width / 2, self.center_y - self.height / 2,
+                         self.center_x - self.width / 2, self.center_y + self.height / 2,
+                         color, self.button_height)
+
+        text_x = self.center_x
+        text_y = self.center_y
+        if not self.pressed:
+            text_x -= self.button_height
+            text_y += self.button_height
+
+        arcade.draw_text(self.text, text_x, text_y,
+                         arcade.color.BLACK, font_size=self.font_size,
+                         width=self.width, align="center",
+                         anchor_x="center", anchor_y="center")
+
+
+class ButtonSprite(arcade.Sprite, Button):
+    def __init__(self, filename, scale, center_x, center_y, action_function):
+        arcade.Sprite.__init__(self, filename=filename, center_x=center_x, center_y=center_y, scale=scale)
+
+        Button.__init__(self, center_x=center_x, center_y=center_y, width=self.width, height=self.height,
+                        action_function=action_function)
+
+
+class ActionSprite(ButtonSprite):
+    def __init__(self, filename, scale, center_x, center_y, action_function):
+        super().__init__(filename, scale, center_x, center_y, action_function)
+
+
+# This is for type hinting to also accept SpriteButton instances and not just Sprites.
+T = typing.TypeVar('T', bound=ButtonSprite)
+
+
+class ButtonSpriteList(arcade.SpriteList):
+    def __init__(self):
+        super().__init__()
+
+    def __iter__(self) -> typing.Iterable[T]:
+        return iter(self.sprite_list)
+
+    def check_press(self, x, y):
+        for sp in self.sprite_list:
+            if sp.check_mouse_press(x, y):
+                sp.on_press()
+
+    def check_release(self):
+        for sp in self.sprite_list:
+            if sp.pressed:
+                sp.on_release()
+
+
+
--- arcade_game/main.py
+++ arcade_game/main.py
@@ -1,54 +1,113 @@
 import arcade
-
+from arcade_game.gui_elements import TextBoxButton, ButtonSprite, ButtonSpriteList
+from game_tools.deck import Deck
 
 ACTION_SPACING = 90
 GEISHA_SPACING = 90
+GEISHA_SCALING = 0.28
+CARD_SPACING = 90
+
+assets_path = 'C:\\Users\\PSere\\Desktop\\hanamikoji_game_assets\\'
+cards_path = assets_path+'\\cards\\'
 
 
-class GameWindow(arcade.Window):
-    assets_path = 'C:\\Users\\PSere\\Desktop\\hanamikoji_game_assets'
-    cards = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
+class Game(arcade.Window):
+    geishas = ('A', 'B', 'C', 'D', 'E', 'F', 'G')
     actions = ('secret', 'burn', 'gift', 'comp')
+    cards = tuple('AABBCCDDDEEEFFFFGGGG')
 
     def __init__(self):
         super().__init__(title='Hanamikoji', antialiasing=True)
         arcade.set_background_color(arcade.color.PURPLE_TAUPE)
 
-        self.sprites = arcade.SpriteList()
-        self.texts = []
+        # To store all sprites:
+        self.static_sprites = arcade.SpriteList()
+        self.card_sprites = arcade.SpriteList()
+        self.action_sprites = ButtonSpriteList()
 
-        for i in range(7):
-            geisha = arcade.Sprite(self.assets_path+'\\'+self.cards[i]+'.png', scale=0.28,
-                                   center_x=int(self.width/2 + (i-3) * GEISHA_SPACING), center_y=self.height/2)
-            self.sprites.append(geisha)
+        self.action_functions = [self.secret, self.burn, self.gift, self.comp]
 
-        for i in range(4):
-            asset = self.assets_path+'\\'+self.actions[i]+'.png'
-            action_sprite_player = arcade.Sprite(asset, scale=0.2, center_x=int(self.width/2 + (i-1.5) * ACTION_SPACING),
-                                                 center_y=60)
-            action_sprite_ai = arcade.Sprite(asset, scale=0.2, center_x=int(self.width/2 + (i-1.5) * ACTION_SPACING),
-                                             center_y=self.height-60)
+        # One time button at the start of the game:
+        self.start_button = TextBoxButton(text='Start Game', center_x=self.width-65, center_y=45,
+                                          width=120, height=60, action_function=self.start_game)
 
-            self.sprites.append(action_sprite_player)
-            self.sprites.append(action_sprite_ai)
+        # Load Geisha sprites:
+        [self.static_sprites.append(arcade.Sprite(assets_path+self.geishas[i]+'.png', scale=GEISHA_SCALING,
+                                                  center_x=int(self.width/2 + (i-3) * GEISHA_SPACING),
+                                                  center_y=self.height/2)) for i in range(7)]
 
-    def update(self, delta_time: float):
-        pass
+        # Game logic:
+        self.deck = Deck()
+        self.player_cards = ''
+        self.opponent_cards = ''
+
+        self.started = False
+
+    def start_game(self):
+        print('Started game')
+
+        # GUI ACTIONS:
+        self.started = True
+        for idx, action in enumerate(self.actions):
+            button = ButtonSprite(filename=assets_path+action+'.png', scale=0.2,
+                                  center_x=int(self.width/1.5 + (idx-3) * ACTION_SPACING), center_y=60,
+                                  action_function=self.action_functions[idx])
+
+            self.action_sprites.append(button)
+
+        # GAME LOGIC:
+        self.deck.pull_card()
+        self.player_cards = ''.join([self.deck.pull_card() for _ in range(6)])
+        self.opponent_cards = ''.join([self.deck.pull_card() for _ in range(6)])
+
+        print(f'Player cards: {self.player_cards}')
+        print(f'Opponent cards: {self.opponent_cards}')
+
+        # for idx, pc in enumerate(tuple(self.player_cards)):
+        #     self.load_card(pc, int(self.width/2 + (idx-2.5) * CARD_SPACING), y=150)
+
+    def secret(self):
+        print('secret')
+
+    def burn(self):
+        print('burn')
+
+    def gift(self):
+        print('gift')
+
+    def comp(self):
+        print('comp')
 
     def on_draw(self):
         arcade.start_render()
 
-        self.sprites.draw()
+        self.static_sprites.draw()
+        self.card_sprites.draw()
+        self.action_sprites.draw()
+
+        if not self.started:
+            self.start_button.draw()
 
         arcade.draw_text('YOU', start_x=20, start_y=30, color=arcade.color.WHITE, font_size=25)
         arcade.draw_text('AI', start_x=20, start_y=self.height-30, color=arcade.color.WHITE, font_size=25)
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-        print(x, y)
+        if not self.started:
+            if self.start_button.check_mouse_press(x, y):
+                self.start_button.on_press()
+
+        self.action_sprites.check_press(x, y)
+
+    def on_mouse_release(self, x: float, y: float, button: int, modifiers: int):
+        if not self.started:
+            if self.start_button.pressed:
+                self.start_button.on_release()
+
+        self.action_sprites.check_release()
 
 
 def main():
-    gameWindow = GameWindow()
+    gameWindow = Game()
     arcade.run()
     return 0
 
