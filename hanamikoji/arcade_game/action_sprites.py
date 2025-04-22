2a37d428a5617e374a042d9bd57d5222567d5e02
Samual Pinheiro <31665870+fsmpinheiro@users.noreply.github.com>
2025-04-22 16:03:24 -0300

    Pequenas mudanças
    Ajstes nas em valores chave do tipo string;
    ajuste na função update_usage; divisão do método get_opponent_sprite em mais dois, um para cada player;

--- hanamikoji/arcade_game/action_sprites.py
+++ hanamikoji/arcade_game/action_sprites.py
@@ -8,24 +8,27 @@ class ActionSprite(arcade.Sprite):
     SCALE = 0.2
 
     def __init__(self, action: str, center_x: float, center_y: float, action_function):
-        arcade.Sprite.__init__(self, filename=os.path.join('assets', 'actions', action + '.png'),
+        arcade.Sprite.__init__(self, filename=os.path.join('.', 'assets', 'actions', action + '.png'),
                                center_x=center_x, center_y=center_y, scale=self.SCALE)
 
-        # These booleans control clickability, texture display and selection:
-        self._enabled: bool = False
+        # These booleand controle the click-abality, texture display and selection:
+        self._enable: bool = False
         self._used: bool = False
         self.pressed: bool = False
         self.selected: bool = False
 
-        # These are the disabled and flipped textures: KEEP ORDER
-        self.append_texture(arcade.load_texture(file_name=os.path.join('assets', 'actions', action+'_disabled.png')))
-        self.append_texture(arcade.load_texture(file_name=os.path.join('assets', 'actions', action+'_used.png')))
+        # Here: disabled and flipped textures: KEEP the ORDER
+        self.append_texture(arcade.load_texture(
+            file_name=os.path.join('.', 'assets', 'actions', action + '_disabled.png')))
+        self.append_texture(arcade.load_texture(
+            file_name=os.path.join('.', 'assets', 'actions', action + '_used.png')))
 
         self.action_function = action_function
         self.enabled = True
 
         # the string of the action
         self.value = action
+
 
     @property
     def enabled(self):
@@ -83,7 +86,6 @@ class ActionSprite(arcade.Sprite):
 
 
 class ActionSpriteManager:
-
     action_library = ('secret', 'burn', 'gift', 'comp')
 
     ACTION_HEIGHT = 60
@@ -91,53 +93,50 @@ class ActionSpriteManager:
 
     def __init__(self, parent_window: arcade.Window):
         self.parent_window = parent_window
-        self.actions = {'p1': [], 'p2': []}
+        self.actions = {'player1': [], 'player2': []}
         self.action_indeces = {}
         self.callbacks = [parent_window.secret_pressed, parent_window.burn_pressed, parent_window.gift_pressed,
                           parent_window.comp_pressed]
-        N = len(self.action_library)
-        for idx, a in enumerate(self.action_library):
-            self.actions['p1'].append(ActionSprite(action=a, action_function=self.callbacks[idx],
-                                                   center_x=self.parent_window.width/2 + (idx - (N-1)/2)
-                                                            * self.ACTION_SPACING,
-                                                   center_y=self.ACTION_HEIGHT))
+        size = len(self.action_library)
 
-            self.actions['p2'].append(ActionSprite(action=a, action_function=self.foo,
-                                                   center_x=self.parent_window.width/2 + (idx - (N-1)/2)
-                                                            * self.ACTION_SPACING,
-                                                   center_y=self.parent_window.height - self.ACTION_HEIGHT))
+        for idx, a in enumerate(self.action_library):
+            self.actions['player1'].append(ActionSprite(action=a, action_function=self.callbacks[idx],
+                                                        center_x=self.parent_window.width / 2 + (idx - (size - 1) / 2)
+                                                                 * self.ACTION_SPACING, center_y=self.ACTION_HEIGHT) )
+
+            self.actions['player2'].append(ActionSprite(action=a, action_function=self.foo,
+                                                        center_x=self.parent_window.width / 2 + (idx - (size - 1) / 2)
+                                                                 * self.ACTION_SPACING,
+                                                        center_y=self.parent_window.height - self.ACTION_HEIGHT) )
 
             self.action_indeces[idx] = a
 
-        self.p1_used = []
-        self.p2_used = []
+        self.player1_used = []
+        self.player2_used = []
 
-    def update_usage(self):
-
-        for idx, act_sprite in enumerate(self.player_actions()):
-            if self.action_indeces[idx] in self.p1_used:
-                act_sprite.used = True
-
-        for idx, act_sprite in enumerate(self.opponent_actions()):
-            if self.action_indeces[idx] in self.p2_used:
-                act_sprite.used = True
 
     def p1_use_action(self, act_str: str):
-        self.p1_used.append(act_str)
+        self.player1_used.append(act_str)
 
     def p2_use_action(self, act_str: str):
-        self.p2_used.append(act_str)
+        self.player2_used.append(act_str)
 
     def player_actions(self):
-        for idx, act in enumerate(self.actions['p1']):
+        for idx, act in enumerate(self.actions['player1']):
             yield idx, act
 
     def opponent_actions(self):
-        for act in self.actions['p2']:
+        for act in self.actions['player2']:
             yield act
 
     def get_opponent_sprite(self, index):
-        return self.actions['p2'][index]
+        return self.actions['player2'][index]
+
+    def get_player2_sprite(self, index):
+        return self.actions['player2'][index]
+
+    def get_player1_sprite(self, index):
+        return self.actions['player1'][index]
 
     def foo(self):
         pass
@@ -147,7 +146,7 @@ class ActionSpriteManager:
             c.selected = False
 
     def get_selected_actions(self):
-        return [act for act in self.actions['p1'] if act.selected]
+        return [act for act in self.actions['player1'] if act.selected]
 
     def all_actions(self):
         for k, v in self.actions.items():
@@ -159,21 +158,21 @@ class ActionSpriteManager:
             action.draw()
 
     def mouse_press(self, x, y):
-        for c in self.actions['p1']:
-            c.mouse_press(x, y)
+        for card in self.actions['player1']:
+            card.mouse_press(x, y)
 
     def mouse_release(self):
-        for c in self.actions['p1']:
-            if c.mouse_release():
+        for act in self.actions['player1']:
+            if act.mouse_release():
                 return 1
         return 0
 
     def enable_all(self):
-        for i, a in self.player_actions():
-            if not a.used:
-                a.enabled = True
+        for i, act in self.player_actions():
+            if not act.used:
+                act.enabled = True
 
     def disable_all(self):
-        for i, a in self.player_actions():
-            if not a.used:
-                a.enabled = False
+        for i, act in self.player_actions():
+            if not act.used:
+                act.enabled = False
